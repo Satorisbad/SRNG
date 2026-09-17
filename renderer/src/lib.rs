3ece356ae15327ae6c718dcc3702d735f49088ea
Satorisbad <satoriren23@gmail.com>
2026-09-17 21:01:45 +0200

    refactor(renderer): expose single semantic prepare entrypoint

--- renderer/src/lib.rs
+++ renderer/src/lib.rs
@@ -1,13 +1,9 @@
 mod model;
 mod prepare;
 mod semantic;
-mod semantic_v4;
-mod semantic_v5;
-mod semantic_v6;
-mod semantic_v7;
 
 pub use model::*;
-pub use semantic_v7::prepare_scene;
+pub use semantic::prepare_scene;
 
 #[cfg(feature = "cpu")]
 pub mod cpu;
