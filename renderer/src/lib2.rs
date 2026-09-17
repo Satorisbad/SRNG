52f6bf2d124f73b4662ec1f83335e5d7b35a5c02
Satorisbad <satoriren23@gmail.com>
2026-09-17 19:18:10 +0200

    fix: enable cross-platform radial gradient normalization

--- renderer/src/lib2.rs
+++ renderer/src/lib2.rs
@@ -3,9 +3,10 @@ mod prepare;
 mod semantic;
 mod semantic_v4;
 mod semantic_v5;
+mod semantic_v6;
 
 pub use model::*;
-pub use semantic_v5::prepare_scene;
+pub use semantic_v6::prepare_scene;
 
 #[cfg(feature = "cpu")]
 pub mod cpu;
