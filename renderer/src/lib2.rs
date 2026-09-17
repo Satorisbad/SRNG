edbcf075bfe183b76c0cb9b76067714bdd82a81f
Satorisbad <satoriren23@gmail.com>
2026-09-17 19:00:22 +0200

    feat: enable advanced semantic normalization

--- renderer/src/lib2.rs
+++ renderer/src/lib2.rs
@@ -1,9 +1,10 @@
 mod model;
 mod prepare;
 mod semantic;
+mod semantic_v4;
 
 pub use model::*;
-pub use semantic::prepare_scene;
+pub use semantic_v4::prepare_scene;
 
 #[cfg(feature = "cpu")]
 pub mod cpu;
