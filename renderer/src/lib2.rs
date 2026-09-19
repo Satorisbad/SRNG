8336cd343174ffa1d3533c2873b12ed570c41feb
Satorisbad <satoriren23@gmail.com>
2026-09-19 05:26:16 +0200

    integrate(filter): wire filter graph with font engine

--- renderer/src/lib2.rs
+++ renderer/src/lib2.rs
@@ -5,9 +5,13 @@ mod semantic_v4;
 mod semantic_v5;
 mod semantic_v6;
 mod semantic_v7;
+pub mod filter;
 pub mod font;
+#[cfg(feature = "cpu")]
+mod filter_cpu;
 
 pub use model::*;
+pub use filter::*;
 pub use semantic_v7::prepare_scene;
 
 #[cfg(feature = "cpu")]
