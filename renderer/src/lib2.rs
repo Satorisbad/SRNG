3e993a9a4db6cdc0665982c35314b35811124ff1
Satorisbad <satoriren23@gmail.com>
2026-09-19 21:57:30 +0200

    Route renderer through image normalization layer

--- renderer/src/lib2.rs
+++ renderer/src/lib2.rs
@@ -1,3 +1,4 @@
+mod image;
 mod model;
 mod prepare;
 mod semantic;
@@ -5,14 +6,16 @@ mod semantic_v4;
 mod semantic_v5;
 mod semantic_v6;
 mod semantic_v7;
+mod semantic_v8;
 pub mod filter;
 pub mod font;
 #[cfg(feature = "cpu")]
 mod filter_cpu;
 
+pub use image::*;
 pub use model::*;
 pub use filter::*;
-pub use semantic_v7::prepare_scene;
+pub use semantic_v8::prepare_scene;
 
 #[cfg(feature = "cpu")]
 pub mod cpu;
