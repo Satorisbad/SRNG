d98f7ee2ee85bcdfd211a1658a34db7bd86f3267
Satorisbad <satoriren23@gmail.com>
2026-09-19 05:26:22 +0200

    integrate(filter): compose filter importer with text shaping

--- src/lib.rs
+++ src/lib.rs
@@ -11,6 +11,8 @@ mod svg_base;
 #[path = "svg_v4.rs"]
 mod svg_v4;
 #[path = "svg_final.rs"]
+mod svg_final;
+#[path = "svg_filter_v06.rs"]
 pub mod svg;
 #[path = "runtime_v2.rs"]
 pub mod runtime;
